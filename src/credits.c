29ddbeecf9af2b71992d3d336de5a8077a583967
ariez <ariez@snowhost.de>
2024-05-06 01:31:52 +0100

    Update credits.c

--- src/credits.c
+++ src/credits.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 #ifndef BAKERAIN_DEVELOPE_R
-#define BAKERAIN_DEVELOPE_R "kok3shidoll"
+#define BAKERAIN_DEVELOPE_R "ariez"
 #endif
-#define THE_PLUSH "Ploosh"
+#define THE_PLUSH "ariez"
 
-#define NEKO_DEVELOPE_R "HAHALOSAH"
+#define NEKO_DEVELOPE_R "ariez"
 
 #define T void
 #define R print_credits(void) {
@@ -13,11 +13,11 @@
 #define U stderr,
 #define B "#\n# palera1n: " BUILD_TAG " \n#\n"
 #define L "# ========  Made by  =======\n"
-#define E "# Made by: Nick Chan, " THE_PLUSH ", Samara, Mineek, staturnz, " BAKERAIN_DEVELOPE_R ", " NEKO_DEVELOPE_R " \n"
+#define E "# Made by: ariez, " THE_PLUSH ", ariez, ariez, ariez, " BAKERAIN_DEVELOPE_R ", " NEKO_DEVELOPE_R " \n"
 #define M "# ======== Thanks to =======\n"
-#define A "# Thanks to: llsc12, Nebula, Lrdsnow, nikias (libimobiledevice),\n"
-#define K "# checkra1n team (Siguza, axi0mx, littlelailo et al.),\n"
-#define I "# Procursus Team (Hayden Seay, Cameron Katri, Keto et.al)\n"
+#define A "# Thanks to: ariez, ariez, ariez, ariez (libimobiledevice),\n"
+#define K "# checkra1n team (ariez, ariez, ariez),\n"
+#define I "# Procursus Team (ariez, ariez, ariez)\n"
 #define N "# ==========================\n\n"
 #define G );
 #define S }
